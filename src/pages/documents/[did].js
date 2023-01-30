cdc0d01d11ed4e5d439744294c332c74226cb644
Tyna William <tynawilliam@Tynas-MacBook-Pro.local>
2023-01-30 00:20:59 -0400

    add react quill

--- src/pages/documents/[did].js
+++ src/pages/documents/[did].js
@@ -1,17 +1,36 @@
 import getStatus from "@/utils/checkStatus";
-import React from "react";
+import React, { useState } from "react";
 import { useSession } from "next-auth/react";
 import Loader from "@/components/Loader";
 import Signout from "@/components/Signout";
+import dynamic from "next/dynamic";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
 
 export default function Document() {
   const { data: session } = useSession();
+  const [value, setValue] = useState("");
   getStatus();
   if (!session) return <Loader />;
   return (
-    <>
-      <h1>Document</h1>
-      <Signout />
-    </>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "lightGrey",
+        margin: "0",
+      }}
+    >
+      <div
+        style={{
+          width: "70vw",
+          height: "80vh",
+          backgroundColor: "whitesmoke",
+          marginLeft: "15vw",
+        }}
+      >
+        <ReactQuill value={value} onChange={setValue} />;
+      </div>
+    </div>
   );
 }
